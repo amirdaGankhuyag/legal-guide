e04b8d5597cef65b1d89aeb9328888f9150dfaa1
amirda <amirda.ganbold@gmail.com>
2025-02-24 12:34:50 +0800

    дипломын сэдвээр шинэ react төсөл үүсгэж, tailwind 4.0  тохируулан хуучин төслийг устгалаа

---
+++ legal-guide-react/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className='text-fuchsia-700'>Home</div>;
+};
+
+export default Home;
--- web/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div className='text-fuchsia-700'>Home</div>
-  )
-}
